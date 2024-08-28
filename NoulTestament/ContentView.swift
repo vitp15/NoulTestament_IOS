424876ab50135a798829d0536eb6f30182dbd6bf
vitp15 <vadim2003plamadeala@gmail.com>
2024-08-28 16:14:36 +0300

    Initial commit

---
+++ NoulTestament/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoulTestament
+//
+//  Created by Vadim on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoulTestament/NoulTestamentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoulTestamentApp.swift
+//  NoulTestament
+//
+//  Created by Vadim on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct NoulTestamentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
