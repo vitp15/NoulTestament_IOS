50cd3461b1ed20d0d151c2d609245d8ad087c609
vitp15 <vadim2003plamadeala@gmail.com>
2024-08-30 19:09:19 +0300

    Audio design

--- NoulTestament/App/Pages/AudioView.swift
+++ NoulTestament/App/Pages/AudioView.swift
@@ -11,6 +11,7 @@ struct AudioView: View {
     @Environment(\.presentationMode) var presentationMode
     let book: Book
     let currChapter: Int
+    @State var width : CGFloat = 0
     
     var body: some View {
         VStack {
@@ -20,78 +21,93 @@ struct AudioView: View {
                 }) {
                     Image(.arrow_back)
                 }
-                .padding(EdgeInsets(top: 25, leading: 16, bottom: 0, trailing: 0))
                 Spacer()
             }
-            
-            // Chapter title and number
+            .padding(EdgeInsets(top: 25, leading: 16, bottom: 0, trailing: 0))
             Spacer()
+            
             VStack {
                 Text(book.name)
                     .font(.roboto, size: 40)
                     .fontWeight(.medium)
+                    .minimumScaleFactor(0.75)
                     .foregroundColor(Color(.onSurface))
                 
                 Text(String(currChapter))
                     .font(.roboto, size: 64)
                     .fontWeight(.medium)
+                    .minimumScaleFactor(0.75)
                     .foregroundColor(Color(.onSurface))
                     .padding(.top, 10)
             }
             Spacer()
             
-            // Playback controls
             HStack {
-                // Rewind 5 seconds
-                Button(action: {
-                    // Handle rewind action
-                }) {
-                    Image(systemName: "gobackward.5")
-                        .font(.system(size: 40))
-                        .foregroundColor(.secondary)
-                }
-                
+                Image(.replay_5)
                 Spacer()
-                
-                // Play/Pause button
-                Button(action: {
-                    // Handle play/pause action
-                }) {
-                    Image(systemName: "play.circle.fill")
-                        .font(.system(size: 80))
-                        .foregroundColor(.secondary)
-                }
-                
-                Spacer()
-                
-                // Forward 5 seconds
-                Button(action: {
-                    // Handle forward action
-                }) {
-                    Image(systemName: "goforward.5")
-                        .font(.system(size: 40))
-                        .foregroundColor(.secondary)
-                }
+                Image(.forward_5)
             }
-            .padding(.horizontal, 50)
+            .padding(.horizontal, 16)
+            .padding(.bottom, 32)
             
-            // Progress bar with time
+            HStack(alignment: .bottom, spacing: 0, content: {
+                Image(.previous)
+                Spacer()
+                Image(.play)
+                Spacer()
+                Image(.next)
+            })
+            .padding(.horizontal, 16)
+            .padding(.bottom, 32)
+            
             VStack {
-                Slider(value: .constant(0.2), in: 0...1)
-                    .accentColor(.secondary)
+                ZStack(alignment: .leading) {
+                    Capsule().fill(Color(.tertiaryFixedDim)).frame(height: 7)
+                    Capsule().fill(Color(.tertiary)).frame(width: self.width, height: 7)
+                }
+                .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                            .onChanged({ (value) in
+                                let x = value.location.x
+                                self.width = value.location.x
+                            }).onEnded({ (value) in
+                                let x = value.location.x
+                                let screen = UIScreen.main.bounds.width - 32
+                                let percent = x / screen
+                                //                            self.player.currentTime = Double(percent) * self.player.duration
+                            }))
                 
                 HStack {
                     Text("1:23")
-                        .font(.custom("Roboto-Regular", size: 14))
-                        .foregroundColor(.secondary)
+                        .font(.roboto, size: 16)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color(.tertiary))
                     Spacer()
                     Text("6:44")
-                        .font(.custom("Roboto-Regular", size: 14))
-                        .foregroundColor(.secondary)
+                        .font(.roboto, size: 16)
+                        .fontWeight(.regular)
+                        .foregroundColor(Color(.tertiary))
                 }
             }
-            .padding(.horizontal)
+            .padding(.horizontal, 16)
+            .padding(.bottom, 20)
         }
+        .background(ZStack {
+            Image(.audio_walpaper)
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea([.leading, .trailing, .bottom])
+            
+            Color(.above_walpapers)
+                .edgesIgnoringSafeArea(.all)
+        })
+        .gesture(
+            DragGesture()
+                .onChanged { value in
+                    if value.translation.width > 70 {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                }
+        )
         .navigationBarHidden(true)
     }
 }
